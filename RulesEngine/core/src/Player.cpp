e442ade953f571f03ae1ebe89ec8eb5accd5158b
cursedmerfolk <172984781+cursedmerfolk@users.noreply.github.com>
2024-06-19 15:09:52 -0500

    Add vscode devcontainer and clang-format

---
+++ RulesEngine/core/src/Player.cpp
@@ -0,0 +1,79 @@
+
+
+#include "Player.h"
+
+
+
+namespace Lorcana {
+
+    bool Player::TurnStart()
+    {
+
+        // Ready phase.
+        for (Card& card : field)
+        {
+            DoReadyPhase(card);
+        }
+
+        // Set phase.
+        for (Card& card : field)
+        {
+            DoSetPhase(card);
+        }
+
+        // Draw phase.
+        hand.push_back(deck.back());
+        deck.pop_back();
+
+        return true;
+    }
+
+    bool Player::CanPlay(const Card& card)
+    {
+
+        int numReadyInk = 0;
+        for (const Card& inkedCard : inkwell) {
+            if (inkedCard.isReady) {
+                numReadyInk += 1;
+            }
+        }
+
+    // 	// TODO: enable once Inkwell is implemented.
+    // 	// if (card.Cost > numReadyInk) {
+    // 	//     return false;
+    // 	// }
+
+        return true;
+    }
+    
+    bool Player::DoReadyPhase(Card& card)
+    {
+        // TODO: Effects that would end "at the start of your turn" and "at the start of your next turn" end.
+
+        if (card.canReady)
+        {
+            card.isReady = true;
+        }
+
+        return true;
+    }
+
+    bool Player::DoSetPhase(Card& card)
+    {
+        card.isDry = true;
+
+        // Gain lore from locations.
+        if (card.cardType == CardType::Location)
+        {
+            loreTotal += card.lore;
+        }
+
+        // TODO: Add start of turn effects to the bag.
+
+        //
+        card.canReady = true;
+
+        return true;
+    }
+
+}
