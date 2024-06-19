0c7f5a643ab79c46fe8315a37b8d9ec3096ff68d
cursedmerfolk <172984781+cursedmerfolk@users.noreply.github.com>
2024-06-19 15:49:21 -0500

    try sudo again

--- .devcontainer/ubuntu-2204/install.sh
+++ .devcontainer/ubuntu-2204/install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-apt update -y
-apt install -y git
-apt install -y swig
-apt install -y mingw-w64
-apt install -y default-jdk
-apt install -y python3-dev
-apt install -y clang-format
-apt install -y make
-apt install -y cmake
-apt install -y pkg-config
+sudo apt update -y
+sudo apt install -y git
+sudo apt install -y swig
+sudo apt install -y mingw-w64
+sudo apt install -y default-jdk
+sudo apt install -y python3-dev
+sudo apt install -y clang-format
+sudo apt install -y make
+sudo apt install -y cmake
+sudo apt install -y pkg-config
