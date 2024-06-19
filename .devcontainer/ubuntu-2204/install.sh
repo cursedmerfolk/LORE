f04276333a0ad16c2d98828161a2d55506df019f
cursedmerfolk <172984781+cursedmerfolk@users.noreply.github.com>
2024-06-19 15:26:44 -0500

    Add github Action

---
+++ .devcontainer/ubuntu-2204/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+apt update -y
+apt install -y git
+apt install -y swig
+apt install -y mingw-w64
+apt install -y default-jdk
+apt install -y python3-dev
+apt install -y clang-format
+apt install -y make
+apt install -y cmake
+apt install -y pkg-config
